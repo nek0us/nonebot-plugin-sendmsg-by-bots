3fc642f180db10723c70684e7ddc942bc59492c8
nek0us <1130131059@qq.com>
2023-07-25 18:23:37 +0800

    test

--- nonebot_plugin_sendmsg_by_bots/tools.py
+++ nonebot_plugin_sendmsg_by_bots/tools.py
@@ -4,4 +4,5 @@ from nonebot.adapters.onebot.v11.adapter import Adapter
 async def send_group_forward_msg_by_bots(group_id:int,msg:list):
     bots = nonebot.get_adapter(Adapter).bots
     for bot in bots:
-        await bots[bot].send_group_forward_msg(group_id=int(1231), messages=msg)
+        await bots[bot].send_group_forward_msg(group_id=int(group_id), messages=msg)
+        
